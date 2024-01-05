1f1ea67d7430fa646fdf39751094f39d0e914b85
Santiago Chango <sachango@uce.edu.ec>
2024-01-04 19:37:33 -0500

    taller19

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Contador titulo="Contador1"/>
-  <Contador titulo="fotos"/>
-  <Contador titulo="Hola"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Contador titulo="Contador1" :valor="10" />
+  <Contador titulo="Contador2" :valor="calcularvalor()"/>
 
-
+  <Contador titulo="Contador3" />
 </template>
 
 <script>
-import Contador from './components/Contador.vue'
+import Contador from "./components/Contador.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Contador
-  }
-}
+    Contador,
+  },
+  methods: {
+    calcularvalor() {
+      return 5 + 5 * 10;
+    },
+  },
+};
 </script>
 
 <style>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,31 +1,54 @@
 <template>
   <h2>{{ titulo }}</h2>
   <p>{{ counter }} <sup>2</sup> = {{ obtenerCuadradoComputada }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ obtenerCuadradoComputada}}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ obtenerCuadradoComputada }}</p>
-
+  <button v-on:click="sumar()"> +1 </button>
+  <button v-on:click="restar()"> -1 </button>
 </template>
 
 <script>
 export default {
-  props:["titulo"],
-  data(){
-    return{
-      counter:5,
+  //props: ["titulo","valor"],
+
+  /* props: {
+    titulo: String,
+    valor: Number,
+  }, */
+  props: {
+    titulo: String,
+    valor: {
+      type:Number,
+      required:false,
+      default:100,
+      validator(value){
+        return value>100;
+      }
     }
   },
-  methods:{
-    obtenerCuadrado(){
-      console.log("se calcula el cuadrado")
-      return this.counter*this.counter;
+  data() {
+    return {
+      counter: this.valor,
+    };
+  },
+
+  methods: {
+    obtenerCuadrado() {
+      console.log("se calcula el cuadrado");
+      return this.counter * this.counter;
+    },
+    sumar(){
+      return this.counter++;
+    },
+    restar(){
+      return this.counter--;
     }
   },
-  computed:{
-    obtenerCuadradoComputada(){
-      console.log("se calcula el cuadrado computado")
-      return this.counter*this.counter;
-    }
-  }
+
+  computed: {
+    obtenerCuadradoComputada() {
+      console.log("se calcula el cuadrado computado");
+      return this.counter * this.counter;
+    },
+  },
 };
 </script>
 
@@ -34,4 +57,23 @@ h2 {
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   color: rgb(0, 255, 166);
 }
+
+p{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+button{
+  background: turquoise;
+  border-radius: 5px;
+  border:1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0px 5px;
+  padding: 5px 15px;
+
+}
+button:hover{
+  background: black;
+}
+
 </style>    
